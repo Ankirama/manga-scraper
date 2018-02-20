ae4feb62eecbd265197aee1d1d1c99e0efbc4cd8
Ankirama <fabien.martinez@epitech.eu>
2018-02-20 12:53:38 +0100

    [mangafox]: Fix uri bug to url

--- apis/mangafox/index.js
+++ apis/mangafox/index.js
@@ -289,7 +289,7 @@ function search(url) {
 
 mangafox.search = function(query) {
     let url = baseURI + '/search.php?name_method=cw&name=' + encodeURIComponent(query) + '&type=&author_method=cw&author=&artist_method=cw&artist=&genres[Action]=0&genres[Adult]=0&genres[Adventure]=0&genres[Comedy]=0&genres[Doujinshi]=0&genres[Drama]=0&genres[Ecchi]=0&genres[Fantasy]=0&genres[Gender+Bender]=0&genres[Harem]=0&genres[Historical]=0&genres[Horror]=0&genres[Josei]=0&genres[Martial+Arts]=0&genres[Mature]=0&genres[Mecha]=0&genres[Mystery]=0&genres[One+Shot]=0&genres[Psychological]=0&genres[Romance]=0&genres[School+Life]=0&genres[Sci-fi]=0&genres[Seinen]=0&genres[Shoujo]=0&genres[Shoujo+Ai]=0&genres[Shounen]=0&genres[Shounen+Ai]=0&genres[Slice+of+Life]=0&genres[Smut]=0&genres[Sports]=0&genres[Supernatural]=0&genres[Tragedy]=0&genres[Webtoons]=0&genres[Yaoi]=0&genres[Yuri]=0&released_method=eq&released=&rating_method=eq&rating=&is_completed=&advopts=1';
-    return search(uri);
+    return search(url);
 }
 
 module.exports = mangafox;
